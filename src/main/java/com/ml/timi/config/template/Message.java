68ee31c6b3275f9f8f705fe0b8db6561fa4a152d
Lin <471429994@qq.com>
2021-09-02 17:33:19 +0800

    20210831

--- src/main/java/com/ml/timi/config/template/Message.java
+++ src/main/java/com/ml/timi/config/template/Message.java
@@ -20,7 +20,7 @@ public class Message {
     public static final String ERROR = "数据组装失败";
 
     /**
-     * 数据组装完成
+     * 数据组装成功
      */
     public static final String  Assembling_SUCCESS = "数据组装成功";
     /**
@@ -29,12 +29,12 @@ public class Message {
     public static final String Assembling_ERROR = "数据组装失败";
 
     /**
-     * 数据组装失败
+     * 接口调用成功，响应成功
      */
     public static final String Response_SUCCESS = "接口调用成功，响应成功";
 
     /**
-     * 数据组装失败
+     * 接口调用成功，响应失败
      */
     public static final String Response_ERROR = "接口调用成功，响应失败";
 
